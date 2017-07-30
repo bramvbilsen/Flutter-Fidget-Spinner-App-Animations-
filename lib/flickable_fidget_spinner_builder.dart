0bf9efdec0c9778844df2e60de8af25e97a6292c
Bram Vanbilsen <bramvbilsen@gmail.com>
2017-07-30 05:32:56 +0200

    Added gesture detector

---
+++ lib/flickable_fidget_spinner_builder.dart
@@ -0,0 +1,88 @@
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+
+class FlickableFidgetSpinner extends StatefulWidget {
+
+  FlickableFidgetSpinner({this.url});
+
+  final String url;
+
+  @override
+  FlickableFidgetSpinnerState createState() => new FlickableFidgetSpinnerState();
+}
+
+class FlickableFidgetSpinnerState extends State<FlickableFidgetSpinner> with SingleTickerProviderStateMixin {
+
+  AnimationController _controller;
+  Animation _animation;
+  int spins = 0;
+  int speed = 500;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = new AnimationController(
+      duration: new Duration(milliseconds: 500),
+      vsync: this,
+    );
+
+    _animation = new CurvedAnimation(
+        parent: _controller,
+        curve: Curves.linear,
+    )..addStatusListener((AnimationStatus status) {
+      if (status == AnimationStatus.completed && spins > 0) {
+        if (speed >= 50)
+          speed -= 50;
+        _controller.duration = new Duration(milliseconds: speed);
+        _controller.forward(from: _controller.value == null ? 0.0 : 1 - _controller.value);
+        if (spins > 0)
+          spins--;
+      } else if (status == AnimationStatus.completed && spins <= 0) {
+        speed = 500;
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Center(
+      child: new GestureDetector(
+        onHorizontalDragEnd: (DragEndDetails det) {
+          if (det.velocity.pixelsPerSecond.dx <= 0.0 && !_controller.isAnimating) {
+            print("true");
+            _controller.duration = new Duration(milliseconds: speed);
+            _controller.forward(from: _controller.value == null ? 0.0 : 1 - _controller.value);
+            if (spins > 0) {
+              spins--;
+            }
+          } else if (det.velocity.pixelsPerSecond.dx <= 0.0 && _controller.isAnimating) {
+            spins++;
+          }
+        },
+        onLongPress: () {
+          _controller.stop();
+          spins = 0;
+          speed = 500;
+          print("resetting");
+        },
+        child: new AnimatedBuilder(
+          animation: _animation,
+          child: new Image.network(widget.url),
+          builder: (BuildContext context, Widget child) {
+            return new Transform.rotate(
+              angle: _controller.value * 2.0 * math.PI,
+              child: child,
+            );
+          },
+        ),
+      )
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './fidget_spinner_builder.dart' as fidget;
+import './flickable_fidget_spinner_builder.dart' as flickableFidget;
 
 void main() {
   runApp(new MaterialApp(
@@ -16,11 +17,13 @@ class HomePage extends StatefulWidget {
 class HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
 
   TabController _controller;
+  PageController _ccontroller;
 
   @override
   void initState() {
     super.initState();
     _controller = new TabController(vsync: this, length: 3);
+    _ccontroller = new PageController();
   }
 
   @override
@@ -34,23 +37,35 @@ class HomePageState extends State<HomePage> with SingleTickerProviderStateMixin 
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("Spin Fidgets, Spin!"),
-        bottom: new TabBar(
-          tabs: <Tab>[
-            new Tab(icon: new Icon(Icons.arrow_right)),
-            new Tab(icon: new Icon(Icons.arrow_downward)),
-            new Tab(icon: new Icon(Icons.arrow_left))
-          ],
-          controller: _controller,
-        ),
       ),
-      body: new TabBarView(
-        controller: _controller,
-        children: <Widget>[
-          new fidget.FidgetSpinner(url: "https://cdn.shopify.com/s/files/1/1663/6213/products/3_1024x1024_3ffffb57-dcf8-4051-8c28-b3046c8a0e72.png?v=1491244603"),
-          new fidget.FidgetSpinner(url: "https://www.cleverr.co.za/wp-content/uploads/2017/04/10_e47e1af3-6fef-480d-aa04-7c579e2085f8_530x.png"),
-          new fidget.FidgetSpinner(url: "https://lh3.googleusercontent.com/GG1TqJSJbwvDE8dqA86Y_tRfKKR8KXDBQtqZW0v5uOpbhWa0Ryk6XDSlQlrhTbgGuuo=w170")
+      bottomNavigationBar: new BottomNavigationBar(
+        onTap: (int index) {
+          _ccontroller.jumpToPage(index);
+        },
+        items: <BottomNavigationBarItem> [
+            new BottomNavigationBarItem(
+              title: new Text("First"),
+              icon: new Icon(Icons.arrow_right)
+            ),
+            new BottomNavigationBarItem(
+              title: new Text("Second"),
+              icon: new Icon(Icons.arrow_downward)
+            ),
+            new BottomNavigationBarItem(
+              title: new Text("Last"),
+              icon: new Icon(Icons.arrow_left)
+            )
         ],
       ),
+      body: new PageView(
+        controller: _ccontroller,
+        physics: new NeverScrollableScrollPhysics(),
+        children: <Widget>[
+          new fidget.FidgetSpinner(url: "https://cdn.shopify.com/s/files/1/1663/6213/products/3_1024x1024_3ffffb57-dcf8-4051-8c28-b3046c8a0e72.png?v=1491244603"),
+          new flickableFidget.FlickableFidgetSpinner(url: "http://i.imgur.com/yWtJdi9.png"),
+          new fidget.FidgetSpinner(url: "https://lh3.googleusercontent.com/GG1TqJSJbwvDE8dqA86Y_tRfKKR8KXDBQtqZW0v5uOpbhWa0Ryk6XDSlQlrhTbgGuuo=w170")
+        ],
+      )
     );
   }
 }
